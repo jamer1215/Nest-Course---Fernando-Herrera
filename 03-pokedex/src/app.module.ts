02c2d606d7638938bfa858a3259429f76c8bfffa
Jamal Mohamad <jamalcuent@gmail.com>
2024-09-25 03:33:18 -0400

    instalamos el paquete mongoose para hacer la conexion pertinente de la BD con nuestro framework: Nest

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -2,6 +2,7 @@ import { join } from 'path';//importado - de node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';//importado
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
@@ -9,6 +10,10 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
     rootPath: join(__dirname,'..','public'),
     }),
+
+    //creemos la referencia de la bd S74 - luego lo haremos con vars. de entorno
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
     PokemonModule],
 
 })
