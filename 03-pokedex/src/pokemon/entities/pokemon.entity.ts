433cf8fab1f2a7a8a718474d9cb32dd64631df04
Jamal Mohamad <jamalcuent@gmail.com>
2024-09-26 00:25:46 -0400

    En pokemon entity establecimos la entidad como un documento (Mongo), establecimos algunos constraints con Prop en los atributos, exportamos un esquema de pokemon para la BD y en el modulo importamos con forFeature el schema predefinido en la entidad

--- 03-pokedex/src/pokemon/entities/pokemon.entity.ts
+++ 03-pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -1,2 +1,24 @@
-export class Pokemon {}//no olvides que esto es la representacion de lo que grabamos en la BD - pudiera verse como una tabla
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import {Document} from 'mongoose'//para que la entidad sea un documento - arq. con la que Mongo trabaja
 
+@Schema() //para indicar que esto es un esquema de BD
+export class Pokemon extends Document{//no olvides que esto es la representacion de lo que grabamos en la BD - pudiera verse como una tabla
+    
+    //id:string //Mongo me lo da - no lo pongo
+
+    @Prop({//reglas de integridad
+        unique:true,
+        index:true,
+    })
+    name:string;
+
+    @Prop({//reglas de integridad
+        unique:true,
+        index:true,
+    })
+    no:number;//numero de pokemon
+
+
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);//exporando un esquema que dice a la BD: estas son las definiciones que quiero, estas son las columnas y demás
--- 03-pokedex/src/pokemon/pokemon.module.ts
+++ 03-pokedex/src/pokemon/pokemon.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
+  imports:[
+    //conectando el módulo con la BD - entidad pokemon
+    MongooseModule.forFeature([{
+      name: Pokemon.name,//sale de la extension del documento, no del atributo name como tal
+      schema: PokemonSchema,
+    }])
+
+  ]
 })
 export class PokemonModule {}
