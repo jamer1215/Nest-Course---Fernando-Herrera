2454960c3402c31e922542d30f1b26e9653c091e
Jamal Mohamad <jamalcuent@gmail.com>
2024-10-03 01:41:42 -0400

    Sesion 92: Realizar peticion https desde nest - jurungando el service y creamos una carpeta interface que mediante la solicitud http que hicimos con el endpoint y con la funcion de vs code de paste code as json (con lo devuelto por la peticion get en el postman) se creo automatico las interfaces y seguimos jurungando el seed.service

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,13 +1,36 @@
 //No olvides mi objetivo - albergar la lógica de negocio y seré llamado en el controller
 
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 
 @Injectable()
 export class SeedService {
 
-  executeSeed(){
-    return "Seed executed"
+  private readonly axios: AxiosInstance = axios;//instancia de axios, crea una dependencia en mi servicio - proyecto
+  //hay varias formas de definir esto
+
+  async executeSeed(){
+
+    
+    //hagamos las peticiones http mejor por axios
+    const {data} = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')//quiero es sacar la data
+    //num pokemon y name del pokemon, desestructurando la vaina lo extraemos facil
+    data.results.forEach(({name,url})=>{
+
+      const segments = url.split('/');//con esto veo que la salida puede verse...
+      const no:number= +segments[segments.length-2]
+
+      //console.log({name,url})
+      //console.log(segments);//...[ 'https:', '', 'pokeapi.co', 'api', 'v2', 'pokemon', '1', '' ] - consola al hacer get en postman
+      console.log({name,no})//{ name: 'bulbasaur', no: 1 } - consola al hacer get en postman
+
+    })
+
+    //console.log(fetch)
+
+    return data.results;
   }
   
 }
